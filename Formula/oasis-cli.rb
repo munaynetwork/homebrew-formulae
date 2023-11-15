cbdb8c94f4ffb3b91ddf8c9635e22c823b0ea073
Valentin Bud <valentin.bud@gmail.com>
2023-11-15 12:33:53 +0200

    oasis-cli: update 0.6.0 bottle.
    Signed-off-by: Valentin Bud <valentin.bud@gmail.com>

--- Formula/oasis-cli.rb
+++ Formula/oasis-cli.rb
@@ -1,13 +1,13 @@
 class OasisCli < Formula
   desc "Official CLI for interacting with Oasis Network"
   homepage "https://oasisprotocol.org/"
-  url "https://github.com/oasisprotocol/cli/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "ecd0834e41cb106c136ea00cd05b24915f288c78bfe0c54aa8ef8cb1e8dc9a9e"
+  url "https://github.com/oasisprotocol/cli/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "ed59a5b50538d4ceda745c6b2e9211324731bcbcd5c58962243c25b250298e3e"
   license "Apache-2.0"
   head "https://github.com/oasisprotocol/cli.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/munaynetwork/homebrew-formulae/releases/download/oasis-cli-0.2.0"
+    root_url "https://github.com/munaynetwork/homebrew-formulae/releases/download/oasis-cli-0.6.0"
     sha256 cellar: :any_skip_relocation, monterey:     "4f3db2f99f4a47f590f013d3c56f06f28ed466c06347029beaf69ae8a125d7b9"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "78e5665bb0797e1cd79f9aca483a493002ffc0ce8d143b024b50aa96845818f1"
   end
